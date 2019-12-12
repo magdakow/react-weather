1c493fd9134b4faced1670439f232e5954a228d7
Magda K <kowalikmagdalena92@gmail.com>
2019-12-12 11:53:17 +0100

    Updates

---
+++ precache-manifest.9eb21fdb61b9f7a45b65067394f1ed05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c325312f1404e8462f9c2545f5ce178",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "65711363207ce378cbc9",
+    "url": "/react-weather/static/css/main.bd364ac4.chunk.css"
+  },
+  {
+    "revision": "569b9f2c106c1649350b",
+    "url": "/react-weather/static/js/2.3185565b.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react-weather/static/js/2.3185565b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65711363207ce378cbc9",
+    "url": "/react-weather/static/js/main.3099abc6.chunk.js"
+  },
+  {
+    "revision": "6304ed26c8ff3f3eeb93",
+    "url": "/react-weather/static/js/runtime-main.773570d8.js"
+  }
+]);
